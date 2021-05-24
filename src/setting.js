6c46ac2e0ed59fcaafe2ee5bf1397fb8f0d66426
松永俊輔 <shunsuke@matsunagashunsukenoMacBook-Air.local>
2021-05-24 15:48:18 +0900

    dev setting front design

--- src/setting.js
+++ src/setting.js
@@ -1,13 +1,20 @@
 document.getElementById("start_button").addEventListener("click", (e) => {
     var RT_URL = document.getElementById("RT_URL").value
     var zip_path = document.getElementById("zip_path").value
-    if (RT_URL && zip_path) {
+    var company_name = document.getElementById("bus_company_name").value
+    var interval_time = document.getElementById("interval_time").value
+
+    // const pattern = /^(https|http):\/\/([a-z]{1,}\.|)(qiita\.com)(\/(.*)|\?(.*)|$)$/g;
+
+    if (RT_URL && zip_path && company_name && Number(interval_time) < 15) {
         window.api.set_RT_URL(RT_URL)
         window.close();
     }
 
     if (!RT_URL) document.getElementById("validate_url").style.visibility = "visible"; else document.getElementById("validate_url").style.visibility = "hidden";
     if (!zip_path) document.getElementById("validate_zip").style.visibility = "visible"; else document.getElementById("validate_zip").style.visibility = "hidden";
+    if (!company_name) document.getElementById("validate_name").style.visibility = "visible"; else document.getElementById("validate_name").style.visibility = "hidden";
+    if (Number(interval_time) < 15) document.getElementById("validate_time").style.visibility = "visible"; else document.getElementById("validate_time").style.visibility = "hidden";
 });
 
 document.querySelector("#file_open_button").addEventListener("click", () => {
